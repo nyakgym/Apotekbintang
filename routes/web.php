e175fa56a52d9a1cd23b38442369bd6d13dad5d6
nyakgym <nyakgymnastiarsyah@gmail.com>
2023-06-13 20:38:11 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\IndexController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 //Route::get('/home', function () {
  //   return view('dashboard');
