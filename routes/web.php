832addfbb2cc9c09315e696ae944124cf5841c72
nyakgym <nyakgymnastiarsyah@gmail.com>
2023-07-09 22:02:09 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/laporan', [IndexController::class,'laporan']);
 Route::get('/login', function () {
     return view('home');
 });
+
 // Auth::routes();
 // Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
